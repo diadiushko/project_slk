a66f731782c29b4db870c7161472771eb511b017
fluxick <61364785+fluxick@users.noreply.github.com>
2020-12-27 02:15:51 +0200

    s

--- js/accordion.js
+++ js/accordion.js
@@ -5,13 +5,11 @@ for (const block of accordions) {
     block.addEventListener('click', function () {
         if (block.classList.contains('active')) {
             block.classList.remove('active');
-        }
-        else{
+        } else {
             for (const el of accordions) {
                 el.classList.remove('active');
             }
             this.classList.add('active');
         }
-    })
-
+    })  
 }
